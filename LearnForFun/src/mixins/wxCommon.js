39f8d403e08684e06779d0878d7b2c12761069d4
Vanellope <wangss@buaa.edu.cn>
2020-05-02 01:27:19 +0800

    加上peggy的课表页面和收藏/喜爱等

--- LearnForFun/src/mixins/wxCommon.js
+++ LearnForFun/src/mixins/wxCommon.js
@@ -30,7 +30,7 @@ export default class wxCommon extends wepy.mixin{
 
   checkNum(number) {
     //学号必须全都是数字
-    let numReg = /(^1[2|3|4|5|6|7|8|9]\d{6}$)|(^09\d{7}$)/
+    let numReg = /(^1[1|2|3|4|5|6|7|8|9|0]\d{6}$)|(^09\d{7}$)/
     // 学号
     if (!numReg.test(number)) {
       return false
