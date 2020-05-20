b9b8e34360ee96bb54e52080e96b9429a31b0ed7
Vanellope <wangss@buaa.edu.cn>
2020-05-21 00:34:06 +0800

    聊天记录不缓存+时间格式

--- LearnForFun/src/mixins/wxCommon.js
+++ LearnForFun/src/mixins/wxCommon.js
@@ -45,4 +45,23 @@ export default class wxCommon extends wepy.mixin{
     return format
   }
 
+  
+
+  chatFormatTime() {
+    const formatNumber = n => {
+      n = n.toString()
+      return n[1] ? n : '0' + n
+    }
+    let date = new Date()
+    var year = date.getFullYear()
+    var month = date.getMonth() + 1
+    var day = date.getDate()
+    
+    var hour = date.getHours()
+    var minute = date.getMinutes()
+    var second = date.getSeconds()
+    
+    return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+   }
+
 }
