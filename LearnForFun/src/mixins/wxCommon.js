2d243bd572d3964f58791d9322c952ba2808f96a
Vanellope <wangss@buaa.edu.cn>
2020-05-27 19:12:53 +0800

    修改bug聊天支持emoji

--- LearnForFun/src/mixins/wxCommon.js
+++ LearnForFun/src/mixins/wxCommon.js
@@ -64,4 +64,29 @@ export default class wxCommon extends wepy.mixin{
     return [year, month, day].map(formatNumber).join('-') + ' ' + [hour, minute, second].map(formatNumber).join(':')
    }
 
+   //把utf16的emoji表情字符进行转码成八进制的字符
+   utf16toEntities(str) {
+    var patt = /[\ud800-\udbff][\udc00-\udfff]/g; // 检测utf16字符正则  
+    return str.replace(patt, function (char) {
+        var H, L, code;
+        if (char.length === 2) {
+            H = char.charCodeAt(0); // 取出高位  
+            L = char.charCodeAt(1); // 取出低位  
+            code = (H - 0xD800) * 0x400 + 0x10000 + L - 0xDC00; // 转换算法  
+            return "&#" + code + ";";
+        } else {
+            return char;
+        }
+    });
+}
+
+    //将编码后的八进制的emoji表情重新解码成十六进制的表情字符
+    entitiesToUtf16(str) {
+        return str.replace(/&#(\d+);/g, function (match, dec) {
+            let H = Math.floor((dec - 0x10000) / 0x400) + 0xD800;
+            let L = Math.floor(dec - 0x10000) % 0x400 + 0xDC00;
+            return String.fromCharCode(H, L);
+        });
+    }
+
 }
